fb11fdd0e47f9fe959a43b67ae34dc2547148cbc
OnikurYH <onikuryh@gmail.com>
2019-10-26 23:40:03 +0800

    Pretty files

--- README.md
+++ README.md
@@ -5,13 +5,17 @@
 ## Usage
 
 ```ts
-import solospace from './mod.ts';
+import solospace from "./mod.ts";
 
-const formattedEnglish = solospace('solospace    can  make     a paragraph   looks      better    !');
+const formattedEnglish = solospace(
+  "solospace    can  make     a paragraph   looks      better    !"
+);
 // solospace can make a paragraph looks better!
 
-const fomattedMixedCharacters = solospace('  這個    solospace    可   以  使段落看  起來  更好  ！     良     い  で  す   。  ε٩(๑>    ₃ <)۶з');
-// 這個 solospace 可以使段落看起來更好！良いです。 ε٩(๑> ₃ <)۶з'
+const fomattedMixedCharacters = solospace(
+  "  這個    solospace    可   以  使段落看  起來  更好  ！     良     い  で  す   。  ε٩(๑>    ₃ <)۶з"
+);
+// 這個 solospace 可以使段落看起來更好！良いです。 ε٩(๑> ₃ <)۶з
 ```
 
 You could find more use cases in `test.ts`
@@ -23,4 +27,5 @@ deno test.ts
 ```
 
 ## Copyright and license
+
 Licensed under the MIT license.
--- test.ts
+++ test.ts
@@ -1,31 +1,54 @@
-import { runTests, test } from 'https://deno.land/std/testing/mod.ts';
-import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
-import solospace from './mod.ts';
+import { runTests, test } from "https://deno.land/std/testing/mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import solospace from "./mod.ts";
 
 test(function stringWithExtraSpaces() {
-    const actual = solospace('solospace    can  make     a paragraph   looks      better    !\n   Than good  .  ');
-    assertEquals(actual, 'solospace can make a paragraph looks better!\nThan good.');
+  const actual = solospace(
+    "solospace    can  make     a paragraph   looks      better    !\n   Than good  .  "
+  );
+  assertEquals(
+    actual,
+    "solospace can make a paragraph looks better!\nThan good."
+  );
 });
 
 test(function stringWithoutExtraSpaces() {
-    const actual = solospace('solospace can make a paragraph looks better!\nThan good.');
-    assertEquals(actual, 'solospace can make a paragraph looks better!\nThan good.');
+  const actual = solospace(
+    "solospace can make a paragraph looks better!\nThan good."
+  );
+  assertEquals(
+    actual,
+    "solospace can make a paragraph looks better!\nThan good."
+  );
 });
 
 test(function stringWithExtraSpacesAndComma() {
-    const actual = solospace('solospace    can  make     a paragraph   looks      better    ,   than good  .  ');
-    assertEquals(actual, 'solospace can make a paragraph looks better, than good.');
+  const actual = solospace(
+    "solospace    can  make     a paragraph   looks      better    ,   than good  .  "
+  );
+  assertEquals(
+    actual,
+    "solospace can make a paragraph looks better, than good."
+  );
 });
 
 test(function stringInStringTemplate() {
-    const actual = solospace(`    solospace         can    make a     paragraph    looks    better!
+  const actual = solospace(`    solospace         can    make a     paragraph    looks    better!
                               Than      good  .   `);
-    assertEquals(actual, 'solospace can make a paragraph looks better!\nThan good.');
+  assertEquals(
+    actual,
+    "solospace can make a paragraph looks better!\nThan good."
+  );
 });
 
 test(function stringAreNoNeedSpacesBetweenCharacters() {
-    const actual = solospace('  這個    solospace    可   以  使段落看  起來  更好  ！\n     良     い  で  す   。  ε٩(๑>    ₃ <)۶з');
-    assertEquals(actual, '這個 solospace 可以使段落看起來更好！\n良いです。 ε٩(๑> ₃ <)۶з');
+  const actual = solospace(
+    "  這個    solospace    可   以  使段落看  起來  更好  ！\n     良     い  で  す   。  ε٩(๑>    ₃ <)۶з"
+  );
+  assertEquals(
+    actual,
+    "這個 solospace 可以使段落看起來更好！\n良いです。 ε٩(๑> ₃ <)۶з"
+  );
 });
 
 runTests();
